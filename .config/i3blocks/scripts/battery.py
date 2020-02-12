9b535828b2f70f2b4d38cffe02d2fa1d253d02ab
Daniel Power <me@danielpower.ca>
2020-02-12 00:38:30 -0330

    Formatting

--- .config/i3blocks/scripts/battery.py
+++ .config/i3blocks/scripts/battery.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python
 import os
 
+icons = {
+    "Discharging": ["", "", "", "", "", "", "", "", "", "", "", ""],
+    "Charging":    ["", "", "", "", "", "", "", "", "", "", "", ""],
+}
+
 battery_level_stream = os.popen('cat /sys/class/power_supply/BAT0/capacity')
 battery_level_string = battery_level_stream.read().strip()
 battery_level_int = int(battery_level_string)
 
 battery_status = os.popen('cat /sys/class/power_supply/BAT0/status')
 battery_status_string = battery_status.read().strip()
-
-icons = {
-    "Discharging": ["", "", "", "", "", "", "", "", "", "", "", ""],
-    "Charging":    ["", "", "", "", "", "", "", "", "", "", "", ""],
-}
 
 if battery_status_string == "Unknown":
     icon = ""
