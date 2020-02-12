867bc8e16bb9362cf0b1c7fc5985c9372e53a652
Daniel Power <me@danielpower.ca>
2020-02-12 00:46:11 -0330

    Change volume on scroll

--- .config/i3blocks/scripts/volume.py
+++ .config/i3blocks/scripts/volume.py
@@ -3,7 +3,7 @@ import os
 
 icons = ["奄", "奔", "墳"]
 
-volume_level_stream = os.popen('pamixer --get-volume')
+volume_level_stream = os.popen("pamixer --get-volume")
 volume_level = int(volume_level_stream.read().strip())
 
 if volume_level == 0:
@@ -13,9 +13,15 @@ elif volume_level < 50:
 else:
     icon = "墳"
 
-if 'button' in os.environ:
-    if os.environ['button'] == "1":
-        os.system('$HOME/.scripts/rofi_choose_default_pulse_device.py')
+if "button" in os.environ:
+    if os.environ["button"] == "1":
+        os.system("$HOME/.scripts/rofi_choose_default_pulse_device.py")
+    elif os.environ["button"] == "4":
+        os.system("pamixer -i 5")
+        volume_level = min(volume_level + 5, 100)
+    elif os.environ["button"] == "5":
+        os.system("pamixer -d 5")
+        volume_level = max(volume_level - 5, 0)
 
 print(icon + " " + str(volume_level) + "%")
 
