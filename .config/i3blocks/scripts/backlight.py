c14f2ff985b154b581b1acdb5644dc6ab659dbe0
Daniel Power <me@danielpower.ca>
2020-02-25 20:24:32 -0330

    Add scroll to change backlight brightness

--- .config/i3blocks/scripts/backlight.py
+++ .config/i3blocks/scripts/backlight.py
@@ -6,6 +6,14 @@ icons = ["", "", "", "", "", "", ""]
 backlight_level_stream = os.popen('xbacklight')
 backlight_level = float(backlight_level_stream.read().strip())
 
+if "button" in os.environ:
+    if os.environ["button"] == "4":
+        os.system("xbacklight -inc 5")
+        backlight_level = min(backlight_level + 5, 100)
+    elif os.environ["button"] == "5":
+        os.system("xbacklight -dec 5")
+        backlight_level = max(backlight_level - 5, 0)
+
 icon = icons[int(6 * (backlight_level / 100))]
 print(icon + " " + str(int(backlight_level)) + "%")
 
