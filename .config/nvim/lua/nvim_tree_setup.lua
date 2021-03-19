e2e3e6392bcb0145f8e6275b881e271a938d9641
Daniel Power <me@danielpower.ca>
2021-03-19 10:12:28 -0230

    Add missed file from previous code dump

---
+++ .config/nvim/lua/nvim_tree_setup.lua
@@ -0,0 +1,7 @@
+vim.g.nvim_tree_ignore = { ".git", "node_modules", ".cache", "__pycache" }
+vim.g.nvim_tree_auto_open = 1
+vim.g.nvim_tree_auto_close = 1
+vim.g.nvim_tree_follow = 1
+vim.g.nvim_tree_git_hl = 1
+
+vim.api.nvim_set_keymap('n', '<C-_>', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
