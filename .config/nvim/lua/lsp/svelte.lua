94559f5c7d3252c732d3b871decc33e6f7358a15
Daniel Power <me@danielpower.ca>
2021-03-21 17:35:01 -0230

    Disable svelte built-in formatting in favor of Prettier

--- .config/nvim/lua/lsp/svelte.lua
+++ .config/nvim/lua/lsp/svelte.lua
@@ -1,4 +1,8 @@
 local on_attach = require('lsp.on_attach')
 
 require('lspconfig').svelte.setup({
+  on_attach = function(client, bufnr)
+    client.resolved_capabilities.document_formatting = false
+    on_attach(clinet, bufnr)
+  end
 })
