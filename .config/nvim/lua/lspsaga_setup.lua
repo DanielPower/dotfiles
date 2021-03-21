52ab1268cbf3105db9f245d5294423a0b8b8a9bf
Daniel Power <me@danielpower.ca>
2021-03-21 17:44:35 -0230

    Setup lspsaga

--- .config/nvim/lua/lspsaga_setup.lua
+++ .config/nvim/lua/lspsaga_setup.lua
@@ -1,3 +1,4 @@
-local lspsaga = require('lspsaga')
+require('lspsaga').init_lsp_saga()
 
-lspsaga.init_lsp_saga()
+vim.cmd('nnoremap <silent> ca :Lspsaga code_action<CR>')
+vim.cmd('nnoremap <silent> K :Lspsaga hover_doc<CR>')
