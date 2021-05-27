dd89474c944131f41f34a7c90ce3cefff90c3438
Daniel Power <me@danielpower.ca>
2021-05-26 22:11:58 -0230

    Various updates

--- .config/nvim/lua/setup/nvim_tree.lua
+++ .config/nvim/lua/setup/nvim_tree.lua
@@ -3,5 +3,6 @@ vim.g.nvim_tree_auto_open = 0
 vim.g.nvim_tree_auto_close = 0
 vim.g.nvim_tree_follow = 1
 vim.g.nvim_tree_git_hl = 1
+vim.g.nvim_tree_indent_markers = 1
 
 vim.api.nvim_set_keymap('n', '<C-_>', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
--- .config/nvim/lua/setup/treesitter.lua
+++ .config/nvim/lua/setup/treesitter.lua
@@ -2,6 +2,6 @@ require'nvim-treesitter.configs'.setup {
   ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
   highlight = {
     enable = true,              -- false will disable the whole extension
-    disable = { "svelte" },  -- list of language that will be disabled
+    disable = {},  -- list of language that will be disabled
   },
 }
