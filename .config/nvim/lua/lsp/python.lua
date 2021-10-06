a65ad7435022429508976db5de0d846b1db9b8ea
Daniel Power <me@danielpower.ca>
2021-10-06 15:01:01 -0230

    Remove gradient hack for pyright root detection

--- .config/nvim/lua/lsp/python.lua
+++ .config/nvim/lua/lsp/python.lua
@@ -1,30 +1,7 @@
-local util = require("lspconfig.util")
-
 return {
-	root_dir = function(fname)
-		-- Hack for Gradient
-		local git_ancestor = util.find_git_ancestor(fname)
-		if git_ancestor == "/Users/daniel/Development/gradient" then
-			return git_ancestor
-		end
-
-		-- General root detection copied from lspconfig
-		local root_files = {
-			"pyproject.toml",
-			"setup.py",
-			"setup.cfg",
-			"requirements.txt",
-			"Pipfile",
-			"pyrightconfig.json",
-		}
-		return util.root_pattern(unpack(root_files))(fname)
-			or util.find_git_ancestor(fname)
-			or util.path.dirname(fname)
-	end,
 	settings = {
 		python = {
 			analysis = {
-				useLibraryCodeForTypes = true,
 				typeCheckingMode = "off",
 			},
 		},
