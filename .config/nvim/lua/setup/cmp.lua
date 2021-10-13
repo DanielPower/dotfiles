db4fdeb5bf8587a82f23c9303cdb26bd51211112
Daniel Power <me@danielpower.ca>
2021-10-13 12:51:04 -0230

    Update cmp config

--- .config/nvim/lua/setup/cmp.lua
+++ .config/nvim/lua/setup/cmp.lua
@@ -3,44 +3,28 @@ local luasnip = require("luasnip")
 
 local has_words_before = function()
 	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-	return col ~= 0
-		and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]
-				:sub(col, col)
-				:match("%s")
-			== nil
+	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 
 local tab_mapping = function(fallback)
-	if vim.fn.pumvisible() == 1 then
-		vim.api.nvim_feedkeys(
-			vim.api.nvim_replace_termcodes("<C-n>", true, true, true),
-			"n",
-			true
-		)
-	elseif has_words_before() and luasnip.expand_or_jumpable() then
-		vim.api.nvim_feedkeys(
-			vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true),
-			"",
-			true
-		)
+	if cmp.visible() then
+		cmp.select_next_item()
+	elseif luasnip.expand_or_jumpable() then
+		luasnip.expand_or_jump()
+	elseif has_words_before() then
+		cmp.complete()
 	else
 		fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
 	end
 end
 
-local shift_tab_mapping = function()
-	if vim.fn.pumvisible() == 1 then
-		vim.api.nvim_feedkeys(
-			vim.api.nvim_replace_termcodes("<C-p>", true, true, true),
-			"n",
-			true
-		)
+local shift_tab_mapping = function(fallback)
+	if cmp.visible() then
+		cmp.select_prev_item()
 	elseif luasnip.jumpable(-1) then
-		vim.api.nvim_feedkeys(
-			vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true),
-			"",
-			true
-		)
+		luasnip.jump(-1)
+	else
+		fallback()
 	end
 end
 
@@ -59,7 +43,6 @@ cmp.setup({
 		["<S-Tab>"] = cmp.mapping(shift_tab_mapping, { "i", "s" }),
 		["<CR>"] = cmp.mapping.confirm({
 			behavior = cmp.ConfirmBehavior.Replace,
-			select = true,
 		}),
 	},
 	sources = {
