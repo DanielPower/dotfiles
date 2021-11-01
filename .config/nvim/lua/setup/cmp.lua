fa8654a8e089e6d51440737c0d1079d46ebd185d
Daniel Power <me@danielpower.ca>
2021-11-01 03:49:04 -0230

    Minor cmp changes

--- .config/nvim/lua/setup/cmp.lua
+++ .config/nvim/lua/setup/cmp.lua
@@ -4,7 +4,11 @@ return function()
 
 	local has_words_before = function()
 		local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-		return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+		return col ~= 0
+			and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]
+					:sub(col, col)
+					:match("%s")
+				== nil
 	end
 
 	local tab_mapping = function(fallback)
@@ -44,6 +48,7 @@ return function()
 			["<S-Tab>"] = cmp.mapping(shift_tab_mapping, { "i", "s" }),
 			["<CR>"] = cmp.mapping.confirm({
 				behavior = cmp.ConfirmBehavior.Replace,
+				select = true,
 			}),
 		},
 		sources = {
