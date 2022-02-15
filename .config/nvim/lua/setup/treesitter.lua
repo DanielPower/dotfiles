18e681ab3a060a8b3c626039c876b0229bba014d
Daniel Power <me@danielpower.ca>
2022-02-15 12:08:07 -0330

    Remove Neorg treesitter parser

--- .config/nvim/lua/setup/treesitter.lua
+++ .config/nvim/lua/setup/treesitter.lua
@@ -1,14 +1,5 @@
 return function()
 	local treesitter = require("nvim-treesitter.configs")
-	local parser_configs = require("nvim-treesitter.parsers").get_parser_configs()
-
-	parser_configs.norg = {
-		install_info = {
-			url = "https://github.com/nvim-neorg/tree-sitter-norg",
-			files = { "src/parser.c", "src/scanner.cc" },
-			branch = "main",
-		},
-	}
 
 	treesitter.setup({
 		ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
