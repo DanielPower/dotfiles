f81a1ae431162d480a437fe41d8892f971a7e5c5
Daniel Power <me@danielpower.ca>
2022-05-03 16:45:39 -0230

    Remove unneeded fields from beancount lsp config

--- .config/nvim/lua/lsp/beancount.lua
+++ .config/nvim/lua/lsp/beancount.lua
@@ -2,8 +2,6 @@ return {
 	filetypes = { "beancount" },
 	init_options = {
 		journal_file = "/home/daniel/Documents/beans/ledger.beancount",
-		pythonPath = "python3",
 	},
-	root_patern = "ledger.beancount",
 	single_file_support = true,
 }
