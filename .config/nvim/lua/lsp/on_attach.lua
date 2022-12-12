099c38c94f46efd3ca43d54bfc765c813685d587
Daniel Power <me@danielpower.ca>
2022-12-12 16:48:38 -0330

    Stylua changes

--- .config/nvim/lua/lsp/on_attach.lua
+++ .config/nvim/lua/lsp/on_attach.lua
@@ -5,9 +5,12 @@ vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
 	border = "double",
 })
 
-vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
-	border = "double",
-})
+vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
+	vim.lsp.handlers.signature_help,
+	{
+		border = "double",
+	}
+)
 
 vim.diagnostic.config({
 	virtual_text = true,
