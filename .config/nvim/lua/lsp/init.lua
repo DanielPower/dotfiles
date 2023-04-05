84eeee705464262a789483f6e2fa8a514f1436ba
Daniel Power <daniel@colabsoftware.com>
2023-04-05 13:59:42 -0230

    Cleanup

--- .config/nvim/lua/lsp/init.lua
+++ .config/nvim/lua/lsp/init.lua
@@ -1,10 +1,7 @@
 return {
-	beancount = require("lsp.beancount"),
 	efm = require("lsp.efm"),
 	eslint = require("lsp.eslint"),
-	pyright = require("lsp.pyright"),
 	sumneko_lua = require("lsp.sumneko_lua"),
 	tsserver = require("lsp.tsserver"),
-	graphql = {},
 	jsonls = require("lsp.jsonls"),
 }
