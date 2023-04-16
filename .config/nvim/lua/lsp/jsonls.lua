6f3b0e8c20bd839e7708075827e0dc781b225be1
Daniel Power <daniel@colabsoftware.com>
2023-04-16 03:50:31 -0230

    Setup json schemas

--- .config/nvim/lua/lsp/jsonls.lua
+++ .config/nvim/lua/lsp/jsonls.lua
@@ -1,8 +1,14 @@
 local on_attach = require("lsp.on_attach")
 
 return {
-	on_attach = function(client, bufnr)
-		client.server_capabilities.documentFormattingProvider = false
-		on_attach(client, bufnr)
-	end,
+  settings = {
+    json = {
+      schemas = require("schemastore").json.schemas(),
+      validate = { enable = true },
+    },
+  },
+  on_attach = function(client, bufnr)
+    client.server_capabilities.documentFormattingProvider = false
+    on_attach(client, bufnr)
+  end,
 }
--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -1,24 +1,25 @@
 return {
-	"williamboman/mason.nvim",
-	dependencies = {
-		"williamboman/mason-lspconfig.nvim",
-		"neovim/nvim-lspconfig",
-		"folke/neodev.nvim",
-	},
-	lazy = false,
-	config = function()
-		local on_attach = require("lsp.on_attach")
-		require("neodev").setup()
-		require("mason").setup()
-		require("mason-lspconfig").setup()
-		require("mason-lspconfig").setup_handlers({
-			function(server_name)
-				local config = require("lsp")[server_name] or {}
-				if not config.on_attach then
-					config.on_attach = on_attach
-				end
-				require("lspconfig")[server_name].setup(config)
-			end,
-		})
-	end,
+  "williamboman/mason.nvim",
+  dependencies = {
+    "williamboman/mason-lspconfig.nvim",
+    "neovim/nvim-lspconfig",
+    "folke/neodev.nvim",
+    "b0o/schemastore.nvim",
+  },
+  lazy = false,
+  config = function()
+    local on_attach = require("lsp.on_attach")
+    require("neodev").setup()
+    require("mason").setup()
+    require("mason-lspconfig").setup()
+    require("mason-lspconfig").setup_handlers({
+      function(server_name)
+        local config = require("lsp")[server_name] or {}
+        if not config.on_attach then
+          config.on_attach = on_attach
+        end
+        require("lspconfig")[server_name].setup(config)
+      end,
+    })
+  end,
 }
