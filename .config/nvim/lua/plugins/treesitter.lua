751fb8683b2a353763e5ec7e4f9da3e719ee19cb
Daniel Power <daniel@colabsoftware.com>
2023-04-16 03:35:56 -0230

    Switch comment plugins for jsx comment support

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,11 +1,15 @@
 return {
 	"nvim-treesitter/nvim-treesitter", -- Used for highlighting and indentation
+	dependencies = {
+		"JoosepAlviste/nvim-ts-context-commentstring",
+	},
 	build = ":TSUpdate",
 	config = function()
 		require("nvim-treesitter.configs").setup({
 			ensure_installed = "all",
 			highlight = { enable = true },
 			indent = { enable = true },
+			context_commentstring = { enable = true },
 		})
 	end,
 }
