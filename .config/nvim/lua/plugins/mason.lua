b3eb205d9e030da370d8ee4850f711d6cf6736f3
Daniel Power <daniel@colabsoftware.com>
2023-04-16 03:42:26 -0230

    Neodev plugin for better config development

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -3,10 +3,12 @@ return {
 	dependencies = {
 		"williamboman/mason-lspconfig.nvim",
 		"neovim/nvim-lspconfig",
+		"folke/neodev.nvim",
 	},
 	lazy = false,
 	config = function()
 		local on_attach = require("lsp.on_attach")
+		require("neodev").setup()
 		require("mason").setup()
 		require("mason-lspconfig").setup()
 		require("mason-lspconfig").setup_handlers({
