55ab21f2a425fb6578f7400106572f18f8622fb2
Daniel Power <daniel@colabsoftware.com>
2023-07-14 02:15:25 -0230

    Add copilot support for nvim

---
+++ .config/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,36 @@
+return {
+  "zbirenbaum/copilot.lua",
+  cmd = "Copilot",
+  event = "InsertEnter",
+  config = function()
+    require("copilot").setup({
+      panel = {
+        enabled = false,
+      },
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        debounce = 75,
+        keymap = {
+          accept = "<C-CR>",
+          accept_word = false,
+          accept_line = false,
+          next = "<C-j>",
+          prev = "<C-k>",
+        },
+      },
+      filetypes = {
+        markdown = false,
+        help = false,
+        gitcommit = false,
+        gitrebase = false,
+        hgcommit = false,
+        svn = false,
+        cvs = false,
+        ["."] = false,
+      },
+      copilot_node_command = "node", -- Node.js version must be > 16.x
+      server_opts_overrides = {},
+    })
+  end,
+}
