badb683e7d45e90eec9d55a58c42e17c27797fa7
Daniel Power <daniel@colabsoftware.com>
2023-07-14 11:39:32 -0230

    Enable eslint autofix on save

--- .config/nvim/lua/lsp/eslint.lua
+++ .config/nvim/lua/lsp/eslint.lua
@@ -1,12 +1,21 @@
+local on_attach = require("lsp.on_attach")
+
 return {
-	filetypes = {
-		"javascript",
-		"javascriptreact",
-		"javascript.jsx",
-		"typescript",
-		"typescriptreact",
-		"typescript.tsx",
-		"vue",
-		"graphql",
-	},
+  on_attach = function(client, bufnr)
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      buffer = bufnr,
+      command = "EslintFixAll",
+    })
+    on_attach(client, bufnr)
+  end,
+  filetypes = {
+    "javascript",
+    "javascriptreact",
+    "javascript.jsx",
+    "typescript",
+    "typescriptreact",
+    "typescript.tsx",
+    "vue",
+    "graphql",
+  },
 }
