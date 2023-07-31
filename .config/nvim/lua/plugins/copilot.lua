19989769a0435012b2274823ea9e55a93f73ed0a
Daniel Power <daniel@colabsoftware.com>
2023-07-31 14:57:29 -0230

    Enable copilot for Markdown

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -1,39 +1,39 @@
 return {
-  "zbirenbaum/copilot.lua",
-  cmd = "Copilot",
-  event = "InsertEnter",
-  config = function()
-    require("copilot").setup({
-      panel = {
-        enabled = false,
-      },
-      suggestion = {
-        enabled = true,
-        auto_trigger = true,
-        debounce = 75,
-        keymap = {
-          accept = "<C-CR>",
-          accept_word = false,
-          accept_line = false,
-          next = "<C-j>",
-          prev = "<C-k>",
-        },
-      },
-      filetypes = {
-        ["."] = false,
-        lua = true,
-        javascript = true,
-        javascriptreact = true,
-        typescript = true,
-        typescriptreact = true,
-        svelte = true,
-        python = true,
-        html = true,
-        css = true,
-        scss = true,
-      },
-      copilot_node_command = "node", -- Node.js version must be > 16.x
-      server_opts_overrides = {},
-    })
-  end,
+	"zbirenbaum/copilot.lua",
+	cmd = "Copilot",
+	event = "InsertEnter",
+	config = function()
+		require("copilot").setup({
+			panel = {
+				enabled = false,
+			},
+			suggestion = {
+				enabled = true,
+				auto_trigger = true,
+				debounce = 75,
+				keymap = {
+					accept = "<C-CR>",
+					accept_word = false,
+					accept_line = false,
+					next = "<C-j>",
+					prev = "<C-k>",
+				},
+			},
+			filetypes = {
+				lua = true,
+				javascript = true,
+				javascriptreact = true,
+				typescript = true,
+				typescriptreact = true,
+				svelte = true,
+				python = true,
+				html = true,
+				css = true,
+				scss = true,
+				markdown = true,
+			},
+			copilot_node_command = "node", -- Node.js version must be > 16.x
+			server_opts_overrides = {},
+		})
+	end,
 }
