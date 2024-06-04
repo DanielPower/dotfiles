4db38d052a90157af49f0a50f0dafeb05c371fe9
Daniel Power <me@danielpower.ca>
2024-06-04 11:49:45 -0230

    Add color schemes

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,6 +1,7 @@
 return {
-  { "folke/tokyonight.nvim", lazy = true },
-  { "rebelot/kanagawa.nvim", lazy = true },
-  { "rmehri01/onenord.nvim", lazy = true },
-  { "catppuccin/nvim",       lazy = true },
+	{ "folke/tokyonight.nvim", lazy = true },
+	{ "rebelot/kanagawa.nvim", lazy = true },
+	{ "eldritch-theme/eldritch.nvim", lazy = true },
+	{ "rmehri01/onenord.nvim", lazy = true },
+	{ "catppuccin/nvim", lazy = true },
 }
