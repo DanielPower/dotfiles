80fdc2e360c7db73f96243eb27d2e3d899b2b783
Daniel Power <me@danielpower.ca>
2025-01-06 14:11:43 -0330

    Cleanup unused colorschemes

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,7 +1,3 @@
 return {
-	{ "folke/tokyonight.nvim", lazy = true },
-	{ "rebelot/kanagawa.nvim", lazy = true },
-	{ "eldritch-theme/eldritch.nvim", lazy = true },
-	{ "rmehri01/onenord.nvim", lazy = true },
-	{ "catppuccin/nvim", lazy = true },
+  { "folke/tokyonight.nvim", lazy = true },
 }
