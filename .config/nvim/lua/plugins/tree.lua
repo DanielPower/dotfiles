5ed075a6102c641d1a33dfce79738ccb718ab7a2
Daniel Power <me@danielpower.ca>
2026-04-17 18:36:49 -0230

    Remove unused plugins

--- .config/nvim/lua/plugins/tree.lua
+++ .config/nvim/lua/plugins/tree.lua
@@ -124,10 +124,6 @@ return {
 				update_root = false,
 				ignore_list = {},
 			},
-			system_open = {
-				cmd = "",
-				args = {},
-			},
 			diagnostics = {
 				enable = false,
 				show_on_dirs = false,
