f75e8a2d7917db0b314c5610f195cdc4db2bebcc
Daniel Power <me@danielpower.ca>
2026-09-11 21:39:33 -0230

    Switch back to lspconfig

--- .config/nvim/lsp/html.lua
+++
@@ -1,5 +0,0 @@
-return {
-	cmd = { "vscode-html-language-server", "--stdio" },
-	filetypes = { "html", "templ" },
-	single_file_support = true,
-}
---
+++ .config/nvim/plugin/lspconfig.lua
@@ -0,0 +1,6 @@
+vim.pack.add({ "https://github.com/neovim/nvim-lspconfig" })
+
+vim.lsp.enable("tsc")
+vim.lsp.enable("efm")
+vim.lsp.enable("html")
+vim.lsp.enable("lua_ls")
